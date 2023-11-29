53ffecb61ea9534a79f1b801b7c3bc9ca8bb56cf
Silentlay <>
2023-11-29 17:39:36 +0300

    add Homework3/

---
+++ Homework3/task64/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 6, 7, 8"
+*/
+
+
+// using System;
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.Write("Введите число M: ");
+//         int M = Convert.ToInt32(Console.ReadLine());
+
+//         Console.Write("Введите число N: ");
+//         int N = Convert.ToInt32(Console.ReadLine());
+
+//       Console.WriteLine(PrintNumbers(M, N));
+//     }
+
+//     private static string PrintNumbers(int start, int end)
+//     {
+//         // Базовый случай
+//         if (start > end) return string.Empty; // Если start > end, возвращаем пустую строку
+
+//         // Рекурсивный случай
+//         return start + "\t" + PrintNumbers(start + 1, end);
+//     }
+// }
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine($"M = {M}; N = {N}. -> {PrintNumbers(M, N)}  ");
+
+    }
+
+    private static string PrintNumbers(int M, int N)
+    {
+        if (M > N)
+        {
+           return "Некорректный диапазон";
+        }
+
+        string result = "\"";
+        for (int i = M; i <= N; i++)
+        {
+            result += i;
+            if (i < N)
+            {
+                result += ", ";
+            }
+        }
+        result += "\"";
+
+        return result;
+    }
+}
+ 
+ 
---
+++ Homework3/task66/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке
+от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         int result = SumNaturalNumbers(1, 15);
+//         Console.WriteLine($"M = 1; N = 15 -> {result}");
+
+//     }
+
+//     static int SumNaturalNumbers(int m, int n)
+//     {
+//         if (m > n)
+//         {
+//             return 0;
+//         }
+
+//         return m + SumNaturalNumbers(m + 1, n);
+//     }
+// }
+
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        if (M <= 0 || N <= 0)
+        {
+            Console.WriteLine("Числа M и N должны быть больше 0");
+        }
+        else
+        {
+            PrintNumbers(M, N, out string numbersString, out int sum);
+
+            Console.WriteLine($"M = {M}; N = {N} -> {sum}");
+        }
+    }
+
+    private static void PrintNumbers(int M, int N, out string result, out int sum)
+    {
+        if (M > N)
+        {
+            result = "Некорректный диапазон";
+            sum = 0;
+            return;
+        }
+
+        result = "\"";
+        sum = 0;
+
+        for (int i = M; i <= N; i++)
+        {
+            result += i;
+            sum += i;
+
+            if (i < N)
+            {
+                result += ", ";
+            }
+        }
+
+        result += "\"";
+    }
+}
+
+
